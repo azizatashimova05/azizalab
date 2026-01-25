ae1cf28e61f5cf7b9ec195ec58c995df2e5c1fdf
Aziza <aziza.tashimova.05@gmail.com>
2026-01-25 17:36:19 +0500

    Organize python basics

---
+++ Practice1/python_basics/syntax.py
@@ -0,0 +1,7 @@
+# This is a comment.
+# Python uses indentation to indicate a block of code.
+
+if 3 > 2:
+    print("Three is greater than two!")
+
+# If you skip the indentation, Python will give you a syntax error.
