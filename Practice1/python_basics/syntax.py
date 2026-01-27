c30c23fc3373e85aada332c05430625352c82f3e
azizatashimova05 <aziza.tashimova.05@gmail.com>
2026-01-27 11:20:37 +0500

    Update syntax.py

--- Practice1/python_basics/syntax.py
+++ Practice1/python_basics/syntax.py
@@ -5,3 +5,42 @@ if 3 > 2:
     print("Three is greater than two!")
 
 # If you skip the indentation, Python will give you a syntax error.
+# SYNTAX in Python
+
+# ---------- Example 1 ----------
+# Indentation is required in Python
+
+if 5 > 3:
+    print("Five is greater than three")
+
+# ---------- Example 2 ----------
+# Using indentation with else
+
+x = 10
+if x > 5:
+    print("x is greater than 5")
+else:
+    print("x is not greater than 5")
+
+# ---------- Example 3 ----------
+# Indentation in a loop
+
+for i in range(3):
+    print(i)
+
+# ---------- Example 4 ----------
+# Python does not need semicolons
+
+print("Hello")
+print("Python syntax is simple")
+
+# ---------- Example 5 ----------
+# Variables do not need type declaration
+
+a = 5
+b = "Text"
+c = 3.5
+
+print(a)
+print(b)
+print(c)
